8a8d0d0a0df32345b0a1c257e6997f25667aa200
Elencrissy <cristina.furgala@gmail.com>
2021-11-09 18:20:17 +0100

    Banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,11 +1,12 @@
 import {Component} from "react";
 import '../../style/components/Banner.scss';
+import coast from '../../assets/coast.png';
 
 class Banner extends Component {
     render() {
         return(
             <div className="banner">
-                {/*<img src="../../assets/coast.png" className="banner__img_coast" alt="banner_img_coast" />*/}
+                <img src={coast} className="banner__img_coast" alt="banner_img_coast" />
                 <p className="banner__slogan">Chez vous, partout et ailleurs</p>
             </div>
         )
