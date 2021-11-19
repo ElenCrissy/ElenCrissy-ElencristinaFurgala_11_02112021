6fe4a74e61cfc6884f19e149e25d65c85e8e1346
Elencrissy <cristina.furgala@gmail.com>
2021-11-20 00:48:30 +0100

    Galerie

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -2,8 +2,17 @@ import {Component} from "react";
 import '../../style/components/gallery.scss';
 import previous from '../../assets/previous.png';
 import next from '../../assets/next.png';
+import {createRef} from "react";
 
 class Gallery extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            isCurrentMedia : true
+        }
+        // this.something = createRef()
+    }
+
 
     createPicturesDOM = (pictures) => {
         let list
@@ -13,44 +22,69 @@ class Gallery extends Component{
                     src={picture}
                     alt=""
                     key={`${index}`}
+                    className={this.state.isCurrentMedia ? "active" : "hidden"}
+                    // ref={this.something}
                 />
             ));
-            // list[0].classList.add("active")
-            // console.log(list[0])
+            console.log(list)
             return list
         }
     }
 
-    previous = (pictures) => {
+    // createPictureDOM = (source) => {
+    //     if(source) {
+    //         return(
+    //             <img
+    //                 src={source}
+    //                 alt=""
+    //             />
+    //         )
+    //     }
+    // }
 
-    }
 
-    next = (pictures) => {
+    // previous = (pictures) => {
+    //
+    // }
+    //
+    // next = (pictures) => {
+    //
+    // }
 
-    }
-
+    // componentDidMount() {
+    //     const picturesArray = this.props.pictures
+    //     this.setState({currentMedia : picturesArray[0]})
+    //     const currentMedia = this.state.currentMedia
+    //
+    //     console.log(this.state.currentMedia)
+    // }
 
     render(){
         const picturesArray = this.props.pictures
         const picturesDOM = this.createPicturesDOM(picturesArray)
 
-        if(picturesDOM){
+
+        if(picturesArray){
+            // const truc = picturesArray.forEach(picture => {
+            //     const pictureDOM = this.createPictureDOM(picture)
+            // })
+
             return (
                 <div className="gallery">
                     <img
                         src={previous}
                         alt=""
                         className={"nav-previous"}
-                        onClick={(e) => {this.previous(picturesDOM)}}
+                        // onClick={(e) => {this.previous(picturesArray, currentMedia)}}
                     />
-                    <ul>
+                    <div className="images">
                         {picturesDOM}
-                    </ul>
+                    </div>
                     <img
                         src={next}
                         alt=""
                         className={"nav-next"}
-                        onClick={(e) => {this.next(picturesDOM)}}
+                        // onClick={(e) => {this.next(picturesArray, currentMedia)}}
                     />
                 </div>
             )
