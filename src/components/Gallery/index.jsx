f4378e27eea4c472fa4a9b0d182f9ed7a6014242
Elencrissy <cristina.furgala@gmail.com>
2021-11-22 19:53:20 +0100

    Galerie

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -6,67 +6,71 @@ import next from '../../assets/next.png';
 class Gallery extends Component{
     constructor(props) {
         super(props);
+        this.state =
+            {
+                current : [0]
+            }
     }
 
 
     createPicturesDOM = (picture, index) => {
         if(picture) {
             return (
-                <img
-                    src={picture}
-                    alt=""
-                    key={`${index}`}
-                />
+                <div className={index === this.state.current ? 'active' : 'hidden'} key={index}>
+                    {index === this.state.current && (<img src={picture} alt=""/>)}
+                </div>
             )
         }
     }
 
 
-    // previous = (length) => {
-    //     if(this.currentImage === length-1) {
-    //         this.currentImage =- 1
-    //     }
-    // }
-    //
-    // next = (length) => {
-    //     if(this.currentImage === length-1) {
-    //         this.currentImage =+ 1
-    //     }
-    // }
+    previous = (length) => {
+        if(this.state.current === 0) {
+            return this.setState({current : length-1})
+        } else {
+            return this.state.current--
+        }
+    }
+
+    next = (length) => {
+        if(this.state.current === length-1) {
+            // this.state.current =+ 1
+            return this.setState({current : [0]})
+        } else {
+            return this.state.current++
+        }
+    }
 
     render(){
-        // const picturesData = this.props.pictures
-        // const picturesArray = Array.from(picturesData)
-        console.log(this.props.pictures)
-        return null
-        // const length = picturesArray.length
-        // if(picturesData){
-        //     return (
-        //         <div className="gallery">
-        //             <div className="images">
-        //                 {
-        //                     picturesData.map((picture, index) => (
-        //                         this.createPicturesDOM(picture, index)
-        //                     ))
-        //                 }
-        //             </div>
-        //             <img
-        //                 src={previous}
-        //                 alt=""
-        //                 className={"nav-previous"}
-        //                 onClick={(e) => {this.previous(length)}}
-        //             />
-        //             <img
-        //                 src={next}
-        //                 alt=""
-        //                 className={"nav-next"}
-        //                 onClick={(e) => {this.next(length)}}
-        //             />
-        //         </div>
-        //     )
-        // } else {
-        //     return null
-        // }
+        const picturesData = this.props.pictures
+        if(picturesData){
+            const length = picturesData.length
+            return (
+                <div className="gallery">
+                    <div className="images">
+                        {
+                            picturesData.map((picture, index) => (
+                                this.createPicturesDOM(picture, index)
+                            ))
+                        }
+                    </div>
+                    <img
+                        src={previous}
+                        alt=""
+                        className={"nav-previous"}
+                        onClick={(event) => {this.previous(length)}}
+                    />
+                    {/*<img*/}
+                    {/*    src={next}*/}
+                    {/*    alt=""*/}
+                    {/*    className={"nav-next"}*/}
+                    {/*    onClick={this.next(length)}*/}
+                    {/*/>*/}
+                </div>
+            )
+        } else {
+            return null
+        }
     }
 }
 
